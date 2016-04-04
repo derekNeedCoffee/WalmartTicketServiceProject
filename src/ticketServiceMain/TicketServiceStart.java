e387c05db6390f68382a04d02453746def5b92ff
Tao Tang <derektaotang@gmail.com>
2016-04-04 11:39:51 -0400

    Set a while(true) loop in the main instead of call main() methods

--- src/ticketServiceMain/TicketServiceStart.java
+++ src/ticketServiceMain/TicketServiceStart.java
@@ -12,20 +12,23 @@ public class TicketServiceStart {
 	static UtilityService utilityService = new UtilityService();
 	static TicketServiceImp tService = new TicketServiceImp();
 	public static void main(String[] args){
+		while (true){
+			
 		//create a utilityService object
 			System.out.println("What do you want to do next ?"+ "\n"+
 		                        "Enter 1 look up available seats."+"\n"+
 								"Enter 2 temparay hold seats."+"\n"+
 		                        "Enter 3 reserve seats");
-			//check if input are valid number
 			
-				while(!newScanner.hasNextInt()){
+			//check if input are valid number
+			while (!newScanner.hasNextInt()){
 					System.out.println("Please enter an Integer between 1 and 3");
 					newScanner.next();
 				}
 				
 			int choice  =newScanner.nextInt();
-			switch(choice){
+			
+			switch (choice){
 			case 1:
 				getAvailableNums();
 				break;
@@ -37,15 +40,11 @@ public class TicketServiceStart {
 				break;
 			default:
 				System.out.println("Please enter a valid integer 1, 2 or 3");
-				main(null);
 				break;
-				
-
 			}
+		}
 			
 	}
-	
-	
 	
 	private synchronized static void  getAvailableNums(){
 		Scanner newScanner = new Scanner(System.in);
@@ -56,27 +55,23 @@ public class TicketServiceStart {
 							" 3 is Balcony Two level."+"\n");
 		
 		//check if input are valid number
-		
-		while(!newScanner.hasNextInt()){
+		while (!newScanner.hasNextInt()){
 			System.out.println("Please enter an Integer between 0 and 3");
 			newScanner.next();
 		}
-		Integer venueLevel = newScanner.nextInt();
+			Integer venueLevel = newScanner.nextInt();
 		if(venueLevel<0 || venueLevel>3){
 			System.out.println("Please enter an integer between 0 and 3");
 		}
-		
-		if(tService == null)
-		tService = new TicketServiceImp();
-		
-		System.out.println("Available seats number "+tService.numSeatsAvailable(venueLevel));
-		main(null);
+			if(tService == null)
+			tService = new TicketServiceImp();
+			System.out.println("Available seats number "+tService.numSeatsAvailable(venueLevel));
 	}
 	
 	private synchronized static void holdSeats(){
-		
 		System.out.println("Please enter your email");
 		String email = newScanner.next();
+		
 		//validate user's input email address
 		if(!utilityService.isEmailValid(email)){
 			System.out.println("Please enter a valid email");
@@ -84,9 +79,9 @@ public class TicketServiceStart {
 			email = newScanner.next();
 		}
 		System.out.println("Enter how many seats you want:");
-		//check if input are valid number
 		
-		while(!newScanner.hasNextInt()){
+		//check if input are valid number
+		while (!newScanner.hasNextInt()){
 			System.out.println("Please enter a valid number");
 			newScanner.next();
 		}
@@ -113,14 +108,12 @@ public class TicketServiceStart {
 			System.out.println( " Seat Level is "+ s.getLevelName() + "  row is " +s.getRow() +" seat number " +s.getSeatNum());
 		
 		}
-		
-		
+	
 		}else if(sh.getSeats().size() < seatsNum){    // Not enough seats, set all hold to false, data to null
 			System.out.println("There are not enough seats");
 			clean(sh);
 		}
 		
-		main(null);
 	}
 
 	/**
@@ -128,29 +121,22 @@ public class TicketServiceStart {
 	 */
 	private synchronized static void reservedSeats() {
 		System.out.println("Please enter your seat hold Id");
-		while(!newScanner.hasNextInt()){
+			while (!newScanner.hasNextInt()){
 			System.out.print("Please enter a valid seat hold. It's a number");
 			newScanner.next();
 		}
-		  int seatHoldId = newScanner.nextInt();
-		   System.out.println("Please enter your email");
-		   String yEmail = newScanner.next();
-		   while(!utilityService.isEmailValid(yEmail)){
+			int seatHoldId = newScanner.nextInt();
+			System.out.println("Please enter your email");
+			String yEmail = newScanner.next();
+			while (!utilityService.isEmailValid(yEmail)){
 			   System.out.println("Please enter a valid email ");
 			   newScanner.next();
 		   }
-		 
 			
 			//Reserve  seats based on the seatHoldId 
 			String confirmationCode = tService.reserveSeats(seatHoldId, yEmail);
-			System.out.println("Seats are reserved. Your confirmation code is "+ confirmationCode);
-			main(null);
-		
-		
-		
+			System.out.println("Seats are reserved. Your confirmation code is "+ confirmationCode);	
 	}
-
-
 	
 	/**
 	 * Clean up the hold seat for current seatHold object
